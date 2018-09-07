64dc5d1db7220d1b3b117c0012bc0dc16274f2cd
정승호 <jsh6858@naver.com>
2018-09-08 05:20:18 +0900

    ㅇㅁㄹ

---
+++ DApp/Assets/Scripts/PlayScene/FixedScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedScene : MonoBehaviour {
+
+    public Transform[] _trBottomButtons;
+
+    public void Push_Contract()
+    {
+        Select_Menu(0);
+    }
+
+    public void Push_Hunter()
+    {
+        Select_Menu(1);
+    }
+
+    public void Push_Gacha()
+    {
+        Select_Menu(2);
+    }
+
+    private void Select_Menu(int iMenu)
+    {
+        for(int i=0; i< _trBottomButtons.Length;++i)
+        {
+            _trBottomButtons[i].localPosition = new Vector2(_trBottomButtons[i].localPosition.x, -630f);
+        }
+        _trBottomButtons[iMenu].localPosition = new Vector2(_trBottomButtons[iMenu].localPosition.x, -570f);
+
+
+    }
+}
---
+++ DApp/Assets/Scripts/Quest/Quest_Bottom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest_Bottom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Push_Admission()
+    {
+        Debug.Log("Admission");
+    }
+}
