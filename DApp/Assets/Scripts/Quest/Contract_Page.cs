457e4e47563b39383e9c1a62cbf3b50569f2f093
정승호 <jsh6858@naver.com>
2018-09-08 11:13:32 +0900

    ㅁㄴㅇㄹ

---
+++ DApp/Assets/Scripts/Quest/Contract_Page.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contract_Page : MonoBehaviour {
+
+	public void Push_ShowDetail()
+    {
+        Debug.Log("Show Detail");
+    }
+
+    public void Refresh_ActiveTime()
+    {
+        Debug.Log("Refresh_ActiveTime");
+    }
+
+    public void Refresh_UnActiveTime()
+    {
+        Debug.Log("Refresh_UnActiveTime");
+    }
+}
