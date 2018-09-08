090560d23162412e837482db4d72ce73b34c5bc8
정승호 <jsh6858@naver.com>
2018-09-08 15:10:50 +0900

    ㅏ;'

---
+++ DApp/Assets/Scripts/Inventory/Item_Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Push_ItemWeapon()
+    {
+        Debug.Log("Push_ItemWeapon");
+    }
+}
