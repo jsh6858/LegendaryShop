2f4311c64898c00667431246929d3329f2cb3ba9
Jihye <kanious86@gmail.com>
2018-09-09 13:28:42 +0900

    no message

--- DApp/Assets/Scripts/Gacha_Page.cs
+++ DApp/Assets/Scripts/Gacha_Page.cs
@@ -6,15 +6,34 @@ public class Gacha_Page : MonoBehaviour {
 
     public bool making = false;
 
-	// Use this for initialization
-	void Start () {
+    UILabel timer1, timer2;
+
+    private void Awake()
+    {
+        timer1 = gameObject.transform.Find("Child1").transform.Find("Label").GetComponent<UILabel>();
+        timer2 = gameObject.transform.Find("Child2").transform.Find("Label").GetComponent<UILabel>();
+
+        int time1 = 10000;
+        int time2 = 20000;
+        timer1.text = (time1 / 3600) + "H " + ((time1 % 3600) / 60) + "M " + ((time1 % 3600) % 60) + "S ";
+        timer2.text = (time2 / 3600) + "H " + ((time2 % 3600) / 60) + "M " + ((time2 % 3600) % 60) + "S ";
+
+        GameObject.Find("Tab3Manager").GetComponent<Tab3Manager>().init_data();
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    }
+
+    public void TimeSetting(string text1, string text2)
+    {
+        timer1.text = text1;
+        timer2.text = text2;
+    }
 
     public void Push_ShowMakeWeapon()
     {
--- DApp/Assets/Scripts/Manager/Tab3Manager.cs
+++ DApp/Assets/Scripts/Manager/Tab3Manager.cs
@@ -8,14 +8,39 @@ public class Tab3Manager : MonoBehaviour {
     public GameObject myPage;
     public GameObject _objContract;
 
+    Gacha_Page gacha;
+
+    int time1 = 10000, time2 = 20000;
+    float fTime = 0f;
+    bool bStart = false;
+
     void Awake()
     {
         myPage.SetActive(false);
     }
 
+    public void init_data()
+    {
+        gacha = GameObject.Find("Gacha_Page(Clone)").GetComponent<Gacha_Page>();
+        bStart = true;
+    }
+
     void Update()
     {
+        if (bStart)
+        {
+            fTime += Time.deltaTime;
 
+            if (1f < fTime)
+            {
+                fTime = 0f;
+                --time1;
+                --time2;
+                string text1 = (time1 / 3600) + "H " + ((time1 % 3600) / 60) + "M " + ((time1 % 3600) % 60) + "S ";
+                string text2 = (time2 / 3600) + "H " + ((time2 % 3600) / 60) + "M " + ((time2 % 3600) % 60) + "S ";
+                gacha.TimeSetting(text1, text2);
+            }
+        }
     }
 
     public void CallManager()
@@ -26,6 +51,8 @@ public class Tab3Manager : MonoBehaviour {
         if (null == _objContract)
         {
             GameObject obj = Resources.Load("Gacha/Gacha_Page") as GameObject;
+
+            gacha = obj.GetComponent<Gacha_Page>();
 
             _objContract = GameObject.Instantiate(obj, Vector3.zero, Quaternion.identity, myPage.transform);
         }
